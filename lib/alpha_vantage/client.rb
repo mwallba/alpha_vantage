8b55bc2305ff043b46c65ada7f83434ce5f1a04e
Michael Wallbaum <mwallba@mwallba.io>
2018-02-18 18:18:29 +0100

    Correct request made by stock method

--- lib/alpha_vantage/client.rb
+++ lib/alpha_vantage/client.rb
@@ -1,9 +1,24 @@
+require 'uri'
+require 'net/http'
+
 module AlphaVantage
   class Client
+    ENDPOINT = 'https://www.alphavantage.co/'.freeze
+
     def initialize(api_key:)
       @api_key = api_key
     end
 
-    def stock(symbol); end
+    def stock(symbol)
+      uri = URI(ENDPOINT)
+      params = {
+        function: 'TIME_SERIES_INTRADAY',
+        symbol: 'MSFT',
+        interval: '1min',
+        api_key: @api_key
+      }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+    end
   end
 end
--- spec/alpha_vantage/client_spec.rb
+++ spec/alpha_vantage/client_spec.rb
@@ -11,6 +11,16 @@ module AlphaVantage
     describe '.stock' do
       describe 'default behaviour' do
         it 'retrieves the intraday time-series data for the given symbol' do
+
+          stub_request(:get, "https://www.alphavantage.co/?api_key=random_api_key&function=TIME_SERIES_INTRADAY&interval=1min&symbol=MSFT").
+            with(  headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Host'=>'www.alphavantage.co',
+            'User-Agent'=>'Ruby'
+          }).
+          to_return(status: 200, body: "", headers: {})
+
           client = AlphaVantage::Client.new(api_key: 'random_api_key')
 
           stock_date = client.stock('MSFT')
